f35308b8da4a21c9007bd9b6c0074263826ef227
AviCh12k <adarsh12k@gmail.com>
2020-02-10 00:47:51 +0530

    Successful

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
 
-
 var inArr = [];
 var str = "";
 var eval_str = "";
-var curr=0,pre="";
-var count=0;
+var curr="",pre="";
+var count=1;
+
+
+// Initially
+
 function initial(){
-    inArr = [];
-    eval_str = "";
-    str = "";
-    document.querySelector("#pre_result_display").textContent = "";
-    document.querySelector("#calc_display").textContent = "";
-    document.querySelector("#curr_result_display").textContent = "";
+    pre = "";                    // Array To store Number and Operators
+    curr="";                 // String representation Of inArr
+    str = "";                      // To store Current Pressed no. or operator
 }
 
+function print(){
+    document.querySelector("#pre_result_display").textContent = pre;
+    document.querySelector("#calc_display").textContent = str;
+    document.querySelector("#curr_result_display").textContent = curr;
+}
 
+initial();
 document.querySelector(".clicking_part").addEventListener("click",e=>{
 
     var input = e.target;
-    if(input.id=="equals")
+    if(input.className == "number" || input.className == "operator")
     {
-        eval_str = inArr.join("");
-        curr = eval(eval_str);
-        document.querySelector("#curr_result_display").textContent = curr;
-        pre = curr;
-        count++;
-    }
-    else if(input.id=="ac")
-    {
-        initial();
-    }
-    else
-    {
-        inArr.push(input.textContent);
-        if(count==0)
+        if(input.id == "equals")
         {
-            document.querySelector("#pre_result_display").textContent = pre;
-            var temp = document.querySelector("#calc_display").textContent;
-            document.querySelector("#calc_display").textContent = temp +  input.textContent;
+            curr = eval(str);
+            print();
+            pre = curr;
+            str = "";
+            count = 1;
         }
         else
         {
-            initial();
-            inArr.push(input.textContent);
-            document.querySelector("#pre_result_display").textContent = pre;
-            var temp = document.querySelector("#calc_display").textContent;
-            document.querySelector("#calc_display").textContent = temp +  input.textContent;
-            count=0;
+            curr = "";
+            str += input.textContent;
+            print();
         }
     }
+    else if(input.id == "ac")
+    {
+        initial();
+        print();
+    }
+    else if(input.id == "back")
+    {
+        str = str.substring(0, str.length-1)
+        print();
+    }
+    else if(input.id == "none")
+    {
+        document.querySelector("#pre_result_display").textContent = "Sorry! I have a Boyfriend.";
+    }
+
+        // if(input.className == "number")
+    // {
+    //     str += input.textContent;
+    // }
+    // else if(input.className == "operator")
+    // {
+    //     inArr.push(Number(str));
+    //     if(input.id == "equals")
+    //     {   
+    //         eval_str = inArr.join();
+    //         eval_str = eval_str.replace(/,/g,"");
+    //         console.log(eval_str);
+    //         console.log(eval(eval_str)); 
+    //     }
+    //     else
+    //     {
+    //         str = input.textContent;
+    //         inArr.push(str);
+    //         str = "";
+    //         console.log(inArr);
+    //     }
+    // }
+    // document.querySelector("#calc_display").textContent = str;
  })
